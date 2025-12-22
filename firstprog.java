a14c2be242c4eee27ccd4ef2f1def55283a9ffd6
Harsha <nanduarvi111@gmail.com>
2025-12-22 21:06:50 +0530

    first java which has basic

---
+++ firstprog.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+public class firstprog{
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+
+System.out.println("Hello world");
+
+
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+ for(int i=1;i<=5;i++){
+    for(int j=1;j<=i;j++){
+        System.out.print("*");
+    }
+    System.out.println();
+
+ }
+   
+    }
+}
