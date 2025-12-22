4e06acd2c0ba1517f5d31d91f3bb7c3a18dc7d23
Harsha <nanduarvi111@gmail.com>
2025-12-22 21:37:01 +0530

    input

--- input2.java
+++ input2.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class input2 {
     public static void main(String[] args) {
-        System.out.println("This is input2.java file");
+        String name;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        name=sc.nextLine();
+        System.out.println("Hello  ,"+ nameNandan);
         
     }
 }
