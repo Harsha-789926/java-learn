b08d3a2a811290876e832746c94331464c6757e0
Harsha <nanduarvi111@gmail.com>
2025-12-25 12:58:01 +0530

    done with taking input

---
+++ inputprg.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class inputprg {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name=sc.nextLine();
+        System.out.println("Welcome  "+name+ "  to java programming");
+    }
+}
+
